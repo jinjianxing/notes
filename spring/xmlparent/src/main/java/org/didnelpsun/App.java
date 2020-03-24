3f9aa7b1568489ab92a1df43f536753efa80547c
Jerome King <2675350965@qq.com>
2020-03-24 11:37:42 +0800

    spring udpate

---
+++ spring/xmlparent/src/main/java/org/didnelpsun/App.java
@@ -0,0 +1,21 @@
+package org.didnelpsun;
+//项目入口
+import org.didnelpsun.test.HelloWorld;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App 
+{
+
+    //获取私有属性，这个属性是应用文档属性
+    private static ApplicationContext welcomeContext;
+    public static void main(String args[]){
+        //将Spring beans配置文件引入，使里面的配置可以被使用
+        welcomeContext = new ClassPathXmlApplicationContext("SpringBeans.xml");
+        HelloWorld hello = (HelloWorld) welcomeContext.getBean("HelloWorldBean");
+        HelloWorld hello2 = (HelloWorld) welcomeContext.getBean("HelloWorldBean2");
+        hello.saySomeThing();
+        hello2.saySomeThing();
+    }
+}
--- spring/xmlparent/src/main/java/org/didnelpsun/test/HelloWorld.java
+++ spring/xmlparent/src/main/java/org/didnelpsun/test/HelloWorld.java
@@ -3,11 +3,14 @@ package org.didnelpsun.test;
 public class HelloWorld {
     // 私有变量words
     private String words;
-    private String user = "Didnelpsun";
+    private String user ;
     //如果我们要对这个类的属性赋值，那么一定要是set开头，如果需要参数就要传入参数
     //参数的配置在resource的xml配置文件中，参数的名字应该对应
     public void setWords(String words){
         this.words = words;
+    }
+    public void setUser(String user){
+        this.user=user;
     }
     public void saySomeThing(){
         System.out.println(this.user +" says "+ this.words);
