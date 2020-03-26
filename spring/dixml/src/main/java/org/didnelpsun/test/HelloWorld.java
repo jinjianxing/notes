3c34e2f184cae6d96664649a255da719f4591aa3
Jerome King <2675350965@qq.com>
2020-03-26 20:13:38 +0800

    update spring

---
+++ spring/dixml/src/main/java/org/didnelpsun/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.didnelpsun.test;
+
+public class HelloWorld {
+    public HelloWorld(){
+        System.out.println("Start building HelloWorld class");
+    }
+    public void SayHello(){
+        System.out.println("Hello,World!");
+    }
+}
