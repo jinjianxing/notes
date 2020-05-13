fcdf020d2aa1e6f0d52d0e08c973eb2709b41480
Didnelpsun <2675350965@qq.com>
2020-05-13 17:14:22 +0800

    spirng模板实例更新

--- spring/couple/src/main/java/org/didnelpsun/App.java
+++ spring/couple/src/main/java/org/didnelpsun/App.java
@@ -1,19 +1,24 @@
+//App.java
 package org.didnelpsun;
 //项目入口
-import org.didnelpsun.test.HelloWorld;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.didnelpsun.Common.Fruit;
+import org.didnelpsun.Factory.FruitFactory;
 
-public class App 
+public class App
 {
-
-    //获取私有属性，这个属性是应用文档属性
-    private static ApplicationContext welcomeContext;
     public static void main(String args[]){
-        //将Spring beans配置文件引入，使里面的配置可以被使用
-        welcomeContext = new ClassPathXmlApplicationContext("SpringBeans.xml");
-        HelloWorld hello = (HelloWorld) welcomeContext.getBean("HelloWorldBean");
-        hello.saySomeThing();
+        Fruit apple = (Fruit) FruitFactory.getFruitFactory("Apple");
+        System.out.println(apple);
+        apple = (Fruit) FruitFactory.getFruitFactory("Apple");
+        System.out.println(apple);
+        Fruit melon = (Fruit) FruitFactory.getFruitFactory("Melon");
+        System.out.println(melon);
+        melon = (Fruit) FruitFactory.getFruitFactory("Melon");
+        System.out.println(melon);
+        Fruit banana = (Fruit) FruitFactory.getFruitFactory("Banana");
+        System.out.println(banana);
+        banana = (Fruit) FruitFactory.getFruitFactory("Banana");
+        System.out.println(banana);
     }
 }
